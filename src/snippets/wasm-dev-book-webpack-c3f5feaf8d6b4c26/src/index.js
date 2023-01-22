3095526aebf663bb7bbb343131534d78282f7821
hikamayogoat <hikamayo.dw@gmail.com>
2023-01-22 22:12:20 +0900

    first

--- src/snippets/wasm-dev-book-webpack-c3f5feaf8d6b4c26/src/index.js
+++
@@ -1,8 +0,0 @@
-export const date_now = Date.now()
-
-import('./wasm_dev_book_webpack').then(module => {
-  const { add, get_timestamp } = module
-  console.log(add(1,2))
-  console.log(get_timestamp())
-})
-
--- src/wasm_dev_book_webpack.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./wasm_dev_book_webpack_bg.wasm";
-export * from "./wasm_dev_book_webpack_bg.js";
